ab96f2e4060e1753dc58306fde95d396a6c67c0e
Shayan Yousefi <shayan.yousefi100@yahoo.com>
2024-08-25 19:51:43 +0330

    feat: integrate Cobra for command handling and add 'hello' test command

---
+++ cmd/hello/main.go
@@ -0,0 +1,33 @@
+package hello
+
+import (
+	"context"
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+type Command struct {
+	name string
+}
+
+func (r *Command) Register(ctx context.Context) *cobra.Command {
+	return &cobra.Command{
+		Use:   "hello [name]",
+		Short: "Say hello",
+		Args:  cobra.MaximumNArgs(1),
+
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) == 0 {
+				r.name = "sotoon"
+			} else {
+				r.name = args[0]
+			}
+
+			r.run(ctx)
+		},
+	}
+}
+
+func (r *Command) run(_ context.Context) {
+	fmt.Println("Hello " + r.name)
+}
